c575d61fda804ac16194bf93507e006a55ba3812
Timothy <128176649+Timothy-P@users.noreply.github.com>
2024-01-22 00:42:52 -0600

    Just added files I have been working on.
    I need sleep and this isn't super fun. However, I listen to music while I do this, so it isn't as bad.

---
+++ script.js
@@ -0,0 +1,154 @@
+var playerstatus;
+var turn = "X";
+var randomNum;
+// Upper row
+const UM = document.getElementById("UM")
+const UR = document.getElementById("UR")
+// Middle row
+const MR = document.getElementById("MR")
+const ML = document.getElementById("ML")
+const MM = document.getElementById("MM")
+// Bottom row
+const BL = document.getElementById("BL")
+const BM = document.getElementById("BM")
+const BR = document.getElementById("BR")
+
+
+
+function pageload() {
+    var loadconf = window.confirm("Do you want to play with a friend?\n(Confirm for yes, Cancel for no.)")
+    if (loadconf == 1) {
+        playerstatus = 1
+    }
+    else {
+        playerstatus = 0
+    }    
+};
+function placeObject(event) {
+    const element = event.target;
+    if (playerstatus === 1) {
+        if (turn == "X") {
+            document.getElementById("turncount").textContent = "It is O\'s turn.";
+            turn = "O";
+            console.log(turn)
+            if (element.textContent === "") {
+                element.textContent = "X"
+            }
+            else {
+                alert("You have chosen to skip your turn.\nPlease pick a valid square next time.")
+            }
+        }
+        else {
+            document.getElementById("turncount").textContent = "It is X\'s turn."
+            turn = "X";
+            console.log(turn)
+            if (element.textContent === "") {
+                element.textContent = "O"
+            }
+            else {
+                alert("You have chosen to skip your turn.\nPlease pick a valid square next time.")
+            }
+        };
+    }
+    else {
+        if (element.textContent === "") {
+            element.textContent = "X";
+            console.log(AI())
+        }
+        else {
+            alert("You have chosen to skip your turn.\nPlease pick a valid square next time.")
+        }
+    }
+    console.log(element)
+};
+function AI() {
+    randomNum = getRandomInt(1, 9)
+    if (randomNum === 1) {
+        if (document.getElementById("UL").textContent === "") {
+            document.getElementById("UL").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 2) {
+        if (document.getElementById("UM").textContent === "") {
+            document.getElementById("UM").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 3) {
+        if (document.getElementById("UR").textContent === "") {
+            document.getElementById("UR").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 4) {
+        if (document.getElementById("ML").textContent === "") {
+            document.getElementById("ML").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 5) {
+        if (document.getElementById("MM").textContent === "") {
+            document.getElementById("MM").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 6) {
+        if (document.getElementById("MR").textContent === "") {
+            document.getElementById("MR").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 7) {
+        if (document.getElementById("BL").textContent === "") {
+            document.getElementById("BL").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 8) {
+        if (document.getElementById("BM").textContent === "") {
+            document.getElementById("BM").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else if (randomNum === 9) {
+        if (document.getElementById("BR").textContent === "") {
+            document.getElementById("BR").textContent = "O"
+        }
+        else {
+            AI()
+        }
+    }
+    else {
+        alert("Something has messed up. Please report this to me.")
+        AI()
+    }
+    return randomNum
+}
+
+// Perm stuff. I'm not gonna touch this any, if at all.
+window.addEventListener("DOMContentLoaded", function() {
+    pageload()
+    document.getElementsByClassName("cell").innerHTML = ""
+});
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max+1);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is inclusive and the minimum is inclusive
+};
